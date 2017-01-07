d412672b51b8340703ecf7141f104890949be235
hydrater <liquid_assassin@hotmail.com>
2017-01-07 14:03:10 +0800

    Initial assets

---
+++ Assets/Networking/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Photon.MonoBehaviour {
+
+	const string VERSION = "Prototype";
+	
+	void Awake()
+	{
+		if (!PhotonNetwork.connected)
+			PhotonNetwork.ConnectUsingSettings(VERSION);
+
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = 20;
+		PhotonNetwork.JoinOrCreateRoom ("Only", roomOptions, TypedLobby.Default);
+	}
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	[PunRPC]
+	void gameStart()
+	{
+		//PhotonNetwork.Instantiate("Player", spawnPoint.position, spawnPoint.rotation, 0);
+	}
+}
