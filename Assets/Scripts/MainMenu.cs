febf9f1beaf55035ec9d0ee95cac2154a9588974
hydrater <liquid_assassin@hotmail.com>
2017-01-10 23:10:53 +0800

    Added main menu and run in background

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : Photon.MonoBehaviour {
+
+	public GameObject nameInput;
+
+	void Awake () 
+	{
+		nameInput.GetComponent<Text>().text = PlayerPrefs.GetString("Player Name");
+		if (nameInput.GetComponent<Text>().text == "")
+			nameInput.GetComponent<Text>().text = string.Format("Player{0}",Random.Range(0,999));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
