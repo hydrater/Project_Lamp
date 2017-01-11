324bfac5c29c20247ee76242837bc9d6270e2b7a
hydrater <liquid_assassin@hotmail.com>
2017-01-11 10:31:35 +0800

    Optimised obj references

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,18 +4,18 @@ using UnityEngine.UI;
 
 public class MainMenu : Photon.MonoBehaviour {
 
-	public GameObject nameInput;
+	public InputField nameInput;
 
 	void Awake () 
 	{
-		nameInput.GetComponent<Text>().text = PlayerPrefs.GetString("Player Name");
-		if (nameInput.GetComponent<Text>().text == "")
-			nameInput.GetComponent<Text>().text = string.Format("Player{0}",Random.Range(0,999));
+		nameInput.text = PlayerPrefs.GetString("Player Name");
+		if (nameInput.text == "")
+			nameInput.text = string.Format("Player{0}",Random.Range(0,999));
 	}
 
 	void Login()
 	{
-		PlayerPrefs.SetString("Player Name", nameInput);
+		PlayerPrefs.SetString("Player Name", nameInput.text);
 	}
 
 
